89287867c3ba9cfdee5c0efff308d4140353f97b
maykicn <maykicn@gmail.com>
2024-04-14 18:52:33 +0200

    Remove log line

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -5,9 +5,6 @@ const Remaining = () => {
   const { budget, expenses } = useContext(AppContext);
   const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
   const remainingBudget = budget - totalExpenses;
-  console.log('remainingBudget', remainingBudget)
-  console.log('budget', budget)
-  console.log('totalExpenses', totalExpenses)
   
     if(totalExpenses > budget) {
         alert("You can not reduce budget value lower than the spending");
